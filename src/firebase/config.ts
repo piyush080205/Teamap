5ef3b35ba5c677af22ebe45692884c1444355392
Nima Sherpa <nimasherpasit@gmail.com>
2026-01-20 21:25:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6758696115-50b0c",
+  "appId": "1:650676516999:web:558623d1288d9f4fc7e022",
+  "apiKey": "AIzaSyCmHsIN65lM6JvFFV54YDccqdgjZUg5jbY",
+  "authDomain": "studio-6758696115-50b0c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "650676516999"
+};
