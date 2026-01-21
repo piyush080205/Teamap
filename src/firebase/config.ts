051ffabee41066421ca065849253c978b643a874
Nima Sherpa <nimasherpasit@gmail.com>
2026-01-21 02:41:18 +0000

    how to manually deploy it using vercel/render guide me

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6758696115-50b0c",
-  "appId": "1:650676516999:web:558623d1288d9f4fc7e022",
-  "apiKey": "AIzaSyCmHsIN65lM6JvFFV54YDccqdgjZUg5jbY",
-  "authDomain": "studio-6758696115-50b0c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "650676516999"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
